2ccb52fc9f5cd23e67c7affba1614b5044810a28
Emmanuel Obeng Twene <delinuxist@gmail.com>
2023-02-28 21:08:49 +0000

    auth refactored

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
-
+import { AuthController } from './controllers';
+import { AuthService } from './services';
 
 @Module({
   providers: [AuthService],
