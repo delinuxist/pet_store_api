2dd849c2bce45e42b1e87d69bdf4139567e0defa
Emmanuel Obeng Twene <delinuxist@gmail.com>
2023-02-28 21:09:53 +0000

    root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { PetModule } from './pets/pets.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, PetModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UsersModule,
+    PetModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
