abb6690236ad1adde386f596b71ed3ac10fa0503
Emmanuel Obeng Twene <delinuxist@gmail.com>
2023-03-07 22:29:10 +0000

    sign in dto

--- src/auth/dtos/sign-in.dto.ts
+++ src/auth/dtos/sign-in.dto.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class SignInDto {
   @IsString()
   @IsNotEmpty()
-  email: string;
+  username: string;
 
   @IsString()
   @IsNotEmpty()
