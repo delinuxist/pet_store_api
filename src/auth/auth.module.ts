0874d2a3f113ead2ce64c42339d7f5e25a352456
Emmanuel Obeng Twene <delinuxist@gmail.com>
2023-03-07 22:28:44 +0000

    auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './controllers';
 import { AuthHelperService, AuthService } from './services';
+import { LocalStrategy } from './strategies';
+import { SessionSerializer } from './strategies/SessionSerializer';
 
 @Module({
-  providers: [AuthService, AuthHelperService],
+  providers: [AuthService, AuthHelperService, LocalStrategy, SessionSerializer],
   controllers: [AuthController],
 })
 export class AuthModule {}
