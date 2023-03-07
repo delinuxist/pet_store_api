f24b6d75b0551dba3722a530102cb3ffa5901f2a
Emmanuel Obeng Twene <delinuxist@gmail.com>
2023-03-07 22:28:16 +0000

    signin guard

--- src/common/guard/signed-in.guard.ts
+++ src/common/guard/signed-in.guard.ts
@@ -1,14 +1,13 @@
 import { CanActivate, ExecutionContext } from '@nestjs/common';
 import { Request } from 'express';
 import { Observable } from 'rxjs';
-import { UserSession } from '../types';
 
 export class SignedInGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const req = context.switchToHttp().getRequest<Request>();
-    const session = req.session as UserSession;
-    return session.user;
+
+    return req.isAuthenticated();
   }
 }
