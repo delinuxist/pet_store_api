10e3bb1e4302910abaf2e8fd33520356c2884b1e
Emmanuel Obeng Twene <delinuxist@gmail.com>
2023-03-08 22:03:44 +0000

    strategy imported and passport module registered

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,16 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { redisModule } from './common/config/modules.config';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    PassportModule.register({
+      session: true,
     }),
     UsersModule,
     PetModule,
