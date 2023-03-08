d4bab1c53c226de58a2e1b9584f118800aae518f
Emmanuel Obeng Twene <delinuxist@gmail.com>
2023-03-08 22:02:52 +0000

    session added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as session from 'express-session';
+import * as connectRedis from 'connect-redis';
+import { Redis } from 'ioredis';
 import { AppModule } from './app.module';
+import * as passport from 'passport';
+import { createClient } from 'redis';
 
 async function bootstrap() {
+  // const redisClient = createClient({ url: process.env.REDIS_URL }).connect();
+  // const RedisStore = connectRedis(session);
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
@@ -12,7 +19,25 @@ async function bootstrap() {
   );
 
   app.setGlobalPrefix('api');
-  app.enableCors();
+  app.enableCors({
+    credentials: true,
+  });
+  app.use(
+    session({
+      // store: new RedisStore({ client: redisClient }),
+      name: 'session_id',
+      secret: 'dark-secret',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 7, //7days
+      },
+    }),
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   const config = new DocumentBuilder()
     .setTitle('Pet API')
