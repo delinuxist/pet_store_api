5be3f26d5debf6aded4febc722a00e0c2197a42c
Emmanuel Obeng Twene <delinuxist@gmail.com>
2023-03-19 19:06:33 +0000

    auth service not completed

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,7 +1,9 @@
 import {
+  ClassSerializerInterceptor,
   ForbiddenException,
   Injectable,
   UnauthorizedException,
+  UseInterceptors,
 } from '@nestjs/common';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { PrismaService } from '../../prisma/prisma.service';
@@ -29,6 +31,7 @@ export class AuthService {
           last_name: userData.last_name,
           first_name: userData.first_name,
           email: userData.email,
+          username: userData.username,
           address: {
             city: userData.address.city,
             town: userData.address.town,
@@ -40,29 +43,32 @@ export class AuthService {
       return user;
     } catch (err) {
       if (err instanceof PrismaClientKnownRequestError) {
-        if (err.code === 'P2002')
-          throw new ForbiddenException('Credential taken');
+        if (err.code === 'P2002' && err.meta?.target[0] === 'username')
+          throw new ForbiddenException('username taken');
       }
+      if (err.code === 'P2002' && err.meta?.target[0] === 'email')
+        throw new ForbiddenException('email taken');
     }
   }
 
-  async signIn(signInData: SignInDto): Promise<UserEntity> {
+  @UseInterceptors(ClassSerializerInterceptor)
+  async validateUser({ username, password }: SignInDto): Promise<UserEntity> {
     // find user with email
     const user = await this.prisma.user.findUnique({
       where: {
-        email: signInData.email,
+        username: username,
       },
     });
 
     if (!user) throw new UnauthorizedException();
 
     const isMatch = await this.authHelperService.comparePassword(
-      signInData.password,
+      password,
       user.hashed_password,
     );
 
     if (!isMatch) throw new UnauthorizedException();
 
-    return user;
+    return isMatch ? user : null;
   }
 }
